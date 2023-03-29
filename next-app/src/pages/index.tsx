30f98ae1ebfab0893e62891bfecd0441deed76a3
adrienlg <90392648+Comcombre@users.noreply.github.com>
2023-03-29 09:34:04 +0200

    ajout suite
    creation du fichier suite.ts ->équivalent d'un controlleur suite
    
    affichage test des tableaux dans l'index

--- next-app/src/pages/index.tsx
+++ next-app/src/pages/index.tsx
@@ -3,16 +3,22 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import App from "@/pages/_app";
-import React from "react";
+import React, {useState} from "react";
 import {ApiCall} from "@/pages/api/hello";
+import {ApiCallSuite} from "@/pages/api/Suite"
 
 
 export default function Home() {
     //Variable qui récupere les données de l'API ,ICI des objets de type user
+    const tab = ApiCallSuite();
+    let [suite,setSuite]=useState<any>([]);
     const users= ApiCall();
     let [user, setUsers] = React.useState<any[]>([]);
     users.then((data: any) => {
         setUsers(data);
+    })
+    tab.then((data)=> {
+        setSuite(data);
     })
   return (
     <>
@@ -20,6 +26,10 @@ export default function Home() {
              {/*Foreach JSON qui parcours les objets de type user et les affiches dans un textarea*/}
              {user.map((p: any) => {
                  return <textarea key={p.id}>{p.username},{p.email}</textarea>
+             })}
+             {/*affichage test des tableau*/}
+             {suite.map((item:any)=>{
+                 return item.suitevalues;
              })}
          </main>
     </>
