d3bbe6b99a13a2443e3b9209d3d8ddeb8de0c801
Sabri <90392579+Sabrrii@users.noreply.github.com>
2023-04-23 14:55:27 +0200

    ajout de Next auth

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,13 +1,22 @@
+import { SessionProvider } from "next-auth/react"
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 // import Font Awesome CSS
 import "@fortawesome/fontawesome-svg-core/styles.css";
-
 import { config } from "@fortawesome/fontawesome-svg-core";
 // Tell Font Awesome to skip adding the CSS automatically
 // since it's already imported above
+
 config.autoAddCss = false;
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// @ts-ignore
+export default function App({
+                              Component,
+                              pageProps: { session, ...pageProps },
+                            }) {
+  return (
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+  )
 }
